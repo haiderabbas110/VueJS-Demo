0bfa5b8be27f30643238236245ab825662b995a7
= <haidersayani95@gmail.com>
2023-11-13 00:09:16 +0500

    chartjs

---
+++ demo-haider/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <Header />
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-3">
+        <Sidebar />
+      </div>
+      <main class="col-sm-9">
+        <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />
+        <Doughnut :data="data.chartConfig" :options="options" />
+
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Header from "./components/partial/header.vue";
+import Sidebar from "./components/partial/sidebar.vue";
+
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  ArcElement
+} from "chart.js";
+import { Bar, Doughnut } from "vue-chartjs";
+import * as chartConfig from './components/Charts/Data/chartConfig.js'
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  ArcElement,
+);
+
+export default {
+  name: "BarChart",
+  components: { Bar, Doughnut, Header, Sidebar },
+  data() {
+    return {
+
+      chartData: {
+        labels: ["January", "February", "March"],
+        datasets: [{ data: [40, 20, 12] }],
+      },
+      chartOptions: {
+        responsive: true,
+      },
+
+      chartConfig,
+    };
+  },
+};
+</script>
---
+++ demo-haider/src/components/partial/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <header>
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img alt="Vue logo" class="logo" src="@/assets/logo.svg" /></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+          </ul>
+          <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+<style scoped>
+header {
+  line-height: 1.5;
+  margin-bottom: 20px;
+  background-color: burlywood;
+}
+
+header img.logo {
+  max-width: 60px;
+  width: 100%;
+}
+</style>
